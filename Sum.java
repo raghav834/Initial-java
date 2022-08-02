6438d68043ff0a1fe9fefc689ac60390ec1b1e30
raghav834 <haktgam@gmail.com>
2022-08-02 09:45:59 +0530

    Sum of Two Numbers

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sum{
+
+public static void main(String arhs[]){
+
+
+Scanner sc = new Scanner(System.in);
+int a,b;
+System.out.println("Enter two numbers to add : ");
+a=sc.nextInt();
+b=sc.nextInt();
+System.out.println("The Sum of "+a+" and "+b+" is "+(a+b));
+
+}
+
+}
